35ac3c4df7a1a70432d48561c72e88c6abbc3315
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2022-01-23 12:07:39 +0100

    Implemented global exception handler and service utility class in the util module

---
+++ product-microservices/util/src/main/java/io/anjola/util/exceptions/http/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package io.anjola.util.exceptions.http;
+
+import io.anjola.util.exceptions.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public @ResponseBody HttpErrorInfo handleNotFoundException(ServerHttpRequest request, Exception ex){
+        return createHttpErrorInfo(HttpStatus.NOT_FOUND, request, ex);
+    }
+
+    private HttpErrorInfo createHttpErrorInfo(HttpStatus httpStatus, ServerHttpRequest request, Exception ex) {
+        final String path = request.getPath().pathWithinApplication().value();
+        final String message = ex.getMessage();
+
+        LOG.debug("Returning HTTP status: {} for path: {}, message: {}", httpStatus, path, message);
+        return new HttpErrorInfo(path, httpStatus, message);
+    }
+}
