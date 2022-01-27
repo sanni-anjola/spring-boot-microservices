047b50ec065f3e79bf494ed52fa07b8fea89b422
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2022-01-27 21:51:46 +0100

    Basic review service with test

--- product-microservices/microservices/review-service/src/main/java/io/anjola/reviewservice/ReviewServiceApplication.java
+++ product-microservices/microservices/review-service/src/main/java/io/anjola/reviewservice/ReviewServiceApplication.java
@@ -2,8 +2,10 @@ package io.anjola.reviewservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("io.anjola")
 public class ReviewServiceApplication {
 
     public static void main(String[] args) {
